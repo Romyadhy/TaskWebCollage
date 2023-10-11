60e2df4a0bdc9b6792b9190304f646b7ebb6e2dc
romyadhy <romyady221@gmail.com>
2023-10-11 22:03:28 +0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3b82f6',
+        'second': '#60a5fa',
+      }
+    },
+  },
+  plugins: [],
+}
+
